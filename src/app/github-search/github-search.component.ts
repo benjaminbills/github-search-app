169c609b3672ae56e00de62c546dc3cc30af5b92
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-04-03 05:43:51 +0300

    add response data to user

--- src/app/github-search/github-search.component.ts
+++ src/app/github-search/github-search.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Search } from '../github-class/search';
+import { User } from '../github-class/user';
+import { GithubService } from '../service/github.service';
 
 @Component({
   selector: 'app-github-search',
   templateUrl: './github-search.component.html',
-  styleUrls: ['./github-search.component.css']
+  styleUrls: ['./github-search.component.css'],
 })
 export class GithubSearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  newSearch = new Search('');
+  user: any;
+  constructor(private githubService: GithubService) {
+    this.user = new User('', '', '', '');
   }
 
+  ngOnInit(): void {}
+  getUser() {
+    this.githubService.getUser2(this.newSearch.search).subscribe((response) => {
+      this.user = response;
+      console.log(this.user);
+
+      // this.user.bio = response.values;
+      // this.user.follower = response.follower;
+      // this.user.login = response.login;
+    });
+    //this.user = this.githubService.user;
+
+    //this.githubService.getUser(this.newSearch.search);
+  }
 }
