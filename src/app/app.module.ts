8506f2da85055bd4720539f2616f496a78580093
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-04-04 23:00:36 +0300

    add generated components to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,15 +9,31 @@ import { GithubSearchComponent } from './github-search/github-search.component';
 import { GithubRepoComponent } from './github-repo/github-repo.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { GithubUserComponent } from './github-user/github-user.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { GithubRepoCardComponent } from './github-repo-card/github-repo-card.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [AppComponent, GithubSearchComponent, GithubRepoComponent, GithubUserComponent],
+  declarations: [
+    AppComponent,
+    GithubSearchComponent,
+    GithubRepoComponent,
+    GithubUserComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    GithubRepoCardComponent,
+  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    FontAwesomeModule,
     FormsModule,
     HttpClientModule,
     NgxPaginationModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
