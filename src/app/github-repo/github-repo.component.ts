5666c54291f08212b9935ce1083f30a19504b0a9
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-04-04 23:02:58 +0300

    reduce complexity of repo card

--- src/app/github-repo/github-repo.component.ts
+++ src/app/github-repo/github-repo.component.ts
@@ -9,19 +9,19 @@ import { GithubService } from '../service/github.service';
 })
 export class GithubRepoComponent implements OnInit {
   newSearch = new Search('');
-  repo: any;
+  repos: any;
   totalRecords!: any;
   page: any = 1;
   maxSize: any;
   constructor(private githubService: GithubService) {
-    this.repo = [];
+    this.repos = [];
   }
 
   ngOnInit(): void {}
 
   getRepo() {
     this.githubService.getRepo2(this.newSearch.search).then(() => {
-      this.repo = this.githubService.repo;
+      this.repos = this.githubService.repo;
     });
   }
 }
