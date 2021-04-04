106b1c84eccc439ac36360355fa4ac9d76a39b15
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-04-04 23:05:34 +0300

    add styling to page and pagination

--- src/app/github-user/github-user.component.ts
+++ src/app/github-user/github-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { Search } from '../github-class/search';
 import { User } from '../github-class/user';
 import { GithubService } from '../service/github.service';
@@ -9,17 +10,31 @@ import { GithubService } from '../service/github.service';
   styleUrls: ['./github-user.component.css'],
 })
 export class GithubUserComponent implements OnInit {
+  faMapMarkerAlt = faMapMarkerAlt;
+
   newSearch = new Search('');
   user: any;
   repo: any;
+  totalRecords!: any;
+  page: any = 1;
+  maxSize: any;
+  showRepo: boolean = false;
+  constructor(private githubService: GithubService) {}
 
-  constructor(private githubService: GithubService) {
-    this.repo = [];
+  ngOnInit(): void {
+    this.showRepo = false;
+    this.githubService.getUser('benjaminbills');
+    this.user = this.githubService.user;
   }
-
-  ngOnInit(): void {}
   getUser() {
+    this.showRepo = false;
     this.githubService.getUser(this.newSearch.search);
     this.user = this.githubService.user;
+    this.repo = this.githubService.repo;
+  }
+  getRepo(username: string) {
+    this.showRepo = true;
+    this.githubService.getUserRepo(username);
+    this.repo = this.githubService.repo;
   }
 }
