2a83690df2928c34938a4daddfc8a0997dbefe3f
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-04-04 23:07:35 +0300

    add toastr functionalities for notification

--- src/app/service/github.service.ts
+++ src/app/service/github.service.ts
@@ -3,15 +3,19 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Repository } from '../github-class/repository';
 import { User } from '../github-class/user';
+import { NotificationService } from './notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GithubService {
   user: User;
-  repo: Repository[] = [];
-  constructor(private http: HttpClient) {
-    this.user = new User('', '', '', '');
+  repo: any;
+  constructor(
+    private http: HttpClient,
+    private notifyService: NotificationService
+  ) {
+    this.user = new User('', '', 0, '', '', '', '');
   }
 
   getUser(username: string) {
@@ -25,15 +29,23 @@ export class GithubService {
           (response) => {
             this.user.avatar_url = response.avatar_url;
             this.user.bio = response.bio;
-            this.user.follower = response.follower;
+            this.user.followers = response.followers;
             this.user.login = response.login;
-
+            this.user.location = response.location;
+            this.user.repos = response.public_repos;
+            this.user.url = response.html_url;
+            if (response.public_repos > 0) {
+              this.repo = this.getUserRepo(response.login);
+              console.log(this.repo);
+              resolve();
+            }
             resolve();
-            console.log(this.user);
+
             console.log(response);
           },
           (error) => {
             console.log(error);
+            this.notifyService.showError(error.error.message, error.status);
           }
         );
     });
@@ -54,17 +66,46 @@ export class GithubService {
     let promise = new Promise<void>((resolve, reject) => {
       this.http
         .get<any>(
-          `${environment.apiUrl}search/repositories?per_page=7&q=${repository}`
+          `${environment.apiUrl}search/repositories?per_page=500&q=${repository}`
         )
         .toPromise()
         .then(
           (response) => {
             this.repo = response.items;
+            console.log(this.repo);
+            if (response.items.length == 0) {
+              this.notifyService.showError(
+                'There are no repository with such name',
+                '404'
+              );
+            }
+            resolve();
+          },
+          (error) => {
+            console.log(error);
+
+            this.notifyService.showError(error.error.message, error.status);
+          }
+        );
+    });
+    return promise;
+  }
+  getUserRepo(username: string) {
+    interface ApiResponse {
+      items: Repository[];
+    }
+    let promise = new Promise<void>((resolve, reject) => {
+      this.http
+        .get<any>(`${environment.apiUrl}users/${username}/repos`)
+        .toPromise()
+        .then(
+          (response) => {
+            this.repo = response;
             resolve();
             console.log('from service:', this.repo);
           },
           (error) => {
-            console.log(error);
+            alert(error);
           }
         );
     });
